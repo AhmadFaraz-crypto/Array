add67a6e44d56e00f026b817961923de57010e18
farazahmadjutt <farazkh.bh@gmail.com>
2019-01-15 02:13:49 -0800

    Itersection in Array in C++

---
+++ Program5.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+int main()
+{
+    int size1, size2, n1[size1], n2[size2], i, j, f;
+    std::cout<<"Enter a first set size ";
+    std::cin>>size1;
+    std::cout<<"Enter a first set values "<<std::endl;
+    for(i=0; i<size1; i++)
+    {
+        std::cin>>n1[i];
+    }
+    std::cout<<"Enter a second set size ";
+    std::cin>>size2;
+    std::cout<<"Enter a second set values "<<std::endl;
+    for(i=0; i<size2; i++)
+    {
+        std::cin>>n2[i];
+    }
+    std::cout<<"\n\n\t\tIntersection values : ";
+    for(i=0; i<size1; i++)
+    {
+        f=0;
+        for(j=0; j<size2; j++)
+        {
+            if(n2[i]==n1[j])
+            {
+                f=1;
+            }
+        }
+        if(l==1)
+        {
+            std::cout<<n2[i];
+        }
+        f=0;
+    }
+}
